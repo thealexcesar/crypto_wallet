23d7faf53e79a93e31cf5857279a64f726a8e3cd
thealexcesar <thealexcesar@gmail.com>
2021-12-03 17:15:31 -0300

    style buttons coins

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,4 @@ class HomeController < ApplicationController
   def index
     @test_calling_params = params[:nome]
   end
-
-  def test
-    if params[:nome].blank?
-      puts "ok"
-    else
-      puts "error"
-    end
-  end
 end
--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,2 +1,3 @@
 class Coin < ApplicationRecord
+
 end
