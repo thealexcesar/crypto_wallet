27f8e17a07dbbcdd91e3eb3907be8e4d6ffbc905
thealexcesar <thealexcesar@gmail.com>
2021-12-03 16:05:29 -0300

    params using query from url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @test_calling_params = params[:nome]
+  end
+
+  def test
+    if params[:nome].blank?
+      puts "ok"
+    else
+      puts "error"
+    end
   end
 end
